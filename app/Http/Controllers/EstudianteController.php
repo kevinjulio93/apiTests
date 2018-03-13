0816634109ec949a39661fb1dba62105eb58101c
kevinjulio93 <kevinjulio93@outlook.com>
2018-03-12 23:58:03 -0500

    first commit

---
+++ app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Models\Estudiante;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class EstudianteController extends Controller
+{
+    
+	
+    public function index()
+    {
+        $estudiante = Estudiante::with("user")->get();
+		return response()->json($estudiante);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario= new User();
+        
+        $usuario->email= $request->email;
+        $usuario->password= Hash::make($request->password);
+        $usuario->save();
+        
+        $estudiante =new Estudiante();
+        $estudiante->name=$request->name;
+        $estudiante->user_id=$usuario->id;
+        $estudiante->last_name=$request->last_name;
+        $estudiante->save();
+        
+        
+        
+        
+		return response()->json(["mensaje"=>"Creado correctamente"]);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $estudiante = Estudiante::find($id);
+        
+        return response()->json($estudiante);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id){
+       	$estudiante = Estudiante::find($id);
+        $estudiante->fill($request->all());
+		$estudiante->save();
+		return response()->json(["mensaje"=>"Actualizacion exitosa"]);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $estudiante = Estudiante::find($id);
+        $estudiante->delete();
+    }
+
+    public function authenticate(Request $request)
+    {
+        $email=$request->email;
+        $password=$request->password;
+        
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+           return response()->json(["auth"=>"true"]);
+        }else{
+            return response()->json(["auth"=>"false"]);
+        }
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+Route::get('home', function () {
+    return 'hola';
+});
+
+Route::resource('estudiante', 'EstudianteController');
+
+Route::get('home', function () {
+    return 'hola';
+});
+
+Route::get('home', function () {
+    return 'hola';
+});
+
+Route::post('login', 'EstudianteController@authenticate');
